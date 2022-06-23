617c852e3f0d94890c4c96bf79b2dedf1da1a27f
Stanislav Skorobogatov <skorobogatovsm@yandex.ru>
2022-06-23 21:43:06 +0400

    First_try_github

---
+++ ep2/1.py
@@ -0,0 +1,3 @@
+q_1, q_2, q_3, q_4 = int(input()), int(input()), int(input()), int(input())
+
+print((q_1+q_2)/(q_3+q_4))
---
+++ ep2/2.py
@@ -0,0 +1,4 @@
+num = int(input())
+
+print("Предыдущее: ", num-1)
+print("Следующее: ", num+1)
